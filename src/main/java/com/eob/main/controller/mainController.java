6d9a2f3f9bd44a9f5fea4ef6d66e67eed5c22050
YYS <yeesol0218@gmail.com>
2025-12-11 14:32:07 +0900

    [main-header] 메인헤더 카테고리 출력 수정
    ajax의 응답으로, List<String>대신 ResponseEntity를 받아 error코드를 수동으로 전송할 수 있게 함

--- src/main/java/com/eob/main/controller/mainController.java
+++ src/main/java/com/eob/main/controller/mainController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,24 +38,25 @@ public class mainController {
     // 헤더 카테고리 항목 불러오기
     @GetMapping("getCategory")
     @ResponseBody
-    public List<String> getCategory() {
+    public ResponseEntity<?> getCategory() {
         // 리턴 객체 선언
         List<String> category = new ArrayList<>();
-        try {            
-            // 카테고리 DB에서 대분류명 List 가져오기
-            Optional<ArrayList<String>> _category = categoryRepository.findByDepth(0);
-            
-            // DB에 값이 존재하고 && Optional객체가 비어있지 않다면
-            if (_category.isPresent() && !_category.get().isEmpty()) {
-                //category에 Optional의 값 모두 add
-                category.addAll(_category.get());
-            }
-            System.out.println("카테고리 불러오기 실행:"+category);
+
+        // 카테고리 DB에서 대분류명 List 가져오기
+        Optional<ArrayList<String>> _category = categoryRepository.findByDepth(0);
+
+        // DB에 값이 존재하고 && Optional객체가 비어있지 않다면
+        if (_category.isPresent() && !_category.get().isEmpty()) {
+            // category에 Optional의 값 모두 add
+            category.addAll(_category.get());
+            System.out.println("카테고리 불러오기 실행:" + category);
             // 카테고리 List보내기
-            return category;
-            
-        } catch (Exception e) {
+            return ResponseEntity.ok(category);
+        } else {
+            // Optional값이 없다면
+            return ResponseEntity.status(500).body("카테고리가 존재하지 않습니다.");
         }
+
     }
 
     // 상점 목록
